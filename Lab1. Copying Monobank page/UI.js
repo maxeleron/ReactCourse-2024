5b607dfd05a637bb07c38bd2d590d58aac57696d
Max Ponomarenko <maxeleron@gmail.com>
2024-03-04 19:54:01 +0200

    Adding JS functionality

--- Lab1. Copying Monobank page/UI.js
+++ Lab1. Copying Monobank page/UI.js
@@ -1,1 +1,5 @@
+// Set Goal
+goalDisplay.textContent = formatNumber(goalValue) + currencySymbol;
 
+// Set Accumulated
+accumulatedDisplay.textContent = formatNumber(accumulatedValue) + currencySymbol;
--- Lab1. Copying Monobank page/main.js
+++ Lab1. Copying Monobank page/main.js
@@ -1,7 +1,43 @@
+// Setting constant numeric values
+const minTransaction = 10;
 const maxTransaction = 29999;
-const moneyInput = document.getElementsByClassName('money-input-contenteditable')[0];
-const moneyInputBlock = document.getElementsByClassName('money-input-block')[0];
+const currencySymbol = ' ₴';
 
+// Getting UI elements
+const moneyInput = document.getElementsByClassName('money-input-contenteditable')[0];
+moneyInput.textContent = +localStorage.getItem("userInput") || 0;;
+const moneyInputBlock = document.getElementsByClassName('money-input-block')[0];
+// Increment buttons
+const add100 = document.getElementsByClassName('chip-button')[0];
+const add500 = document.getElementsByClassName('chip-button')[1];
+const add1000 = document.getElementsByClassName('chip-button')[2];
+// Pay buttons
+const monoPayButton = document.getElementsByClassName('mono-pay')[0];
+const gPayButton = document.getElementsByClassName('tap-to-pay')[0];
+// Jar stats
+const accumulatedDisplay = document.getElementsByClassName('stats-data__value')[0];
+const goalDisplay = document.getElementsByClassName('stats-data__value')[1];
+// Jar stats variables
+let accumulatedValue = +localStorage.getItem("accumulated") || 7700;
+const goalValue = 250000;
+
+// Function to format numbers, so they will be displayed with spaces every 3 digits
+function formatNumber(nr) {
+    return nr.toString()
+      .split('')
+      .reverse()
+      .map((n, idx) => idx % 3 === 0 ? n + ' ' : n)
+      .reverse()
+      .join('')
+      .trim();
+}
+
+function updateAccumulated() {
+    const sum = accumulatedValue + parseInt(moneyInput.textContent);
+    localStorage.setItem("accumulated", sum);
+
+    accumulatedDisplay.textContent = formatNumber(accumulatedValue + +moneyInput.textContent) + currencySymbol;
+}
 
 // MoneyInput will only take numbers
 moneyInput.addEventListener('keypress', function(e) {
@@ -13,12 +49,12 @@ moneyInput.addEventListener('keypress', function(e) {
     // moneyInput.textContent = parseInt(moneyInput.textContent);
 
     // removing class 'empty'
-    if (parseInt(moneyInput.textContent) > 3){
+    if (parseInt(moneyInput.textContent) > minTransaction){
         moneyInputBlock.classList.remove('empty');
         moneyInputBlock.classList.remove('incorrect');
     }
 
-    if (parseInt(moneyInput.textContent) < 3)
+    if (parseInt(moneyInput.textContent) < minTransaction)
         moneyInputBlock.classList.add("incorrect");
 
     if (parseInt(moneyInput.textContent) == 0)
@@ -27,4 +63,24 @@ moneyInput.addEventListener('keypress', function(e) {
     // setting max to input
     if (parseInt(moneyInput.textContent) > maxTransaction)
         moneyInput.textContent = maxTransaction;
+
+    // localStorage.setItem("userInput", moneyInput.textContent);
 });
+
+add100.addEventListener('click', function() {
+    moneyInput.textContent = parseInt(moneyInput.textContent) + 100;
+});
+
+add500.addEventListener('click', function() {
+    moneyInput.textContent = parseInt(moneyInput.textContent) + 500;
+});
+
+add1000.addEventListener('click', function() {
+    moneyInput.textContent = parseInt(moneyInput.textContent) + 1000;
+});
+
+// Extracting 'Acumulated' value
+// monoPayButton.addEventListener('click', updateAccumulated());
+
+// Storing 'Acumulated' value
+// add1000.addEventListener('click', updateAccumulated());
